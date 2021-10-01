86ca1070e7bd7ca6ef11b4fc545d2a920c8df237
Itsjohanes <71767003+Itsjohanes@users.noreply.github.com>
2021-10-01 10:17:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel = "icon" href = "Album/face.png">
+<title>Seputar Kyoto</title>
+
+<link rel = "stylesheet" href = "style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Stick+No+Bills&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style\web.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Grechen+Fuemen&family=Stick+No+Bills&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<div class="header">
+  <img src = "logo edit.png" href = "logo"  height="151px">
+  <h3>All About Kyoto</h3>
+</div>
+
+<div class="navbar">
+<a href = "https://www.instagram.com/johannesap_/">instagram</a>
+<a href="#Album" class="right">Album</a>
+<a href="#about" class="right">About Kyoto</a>
+
+
+
+</div>
+
+<div class="row">
+  <div class="side">
+    <h1>Introduction</h1>
+    <img class = "img" src = "kyoto.png" alt = "kyoto awal">
+    <p>Kyoto is one of the cities in Japan, this city is a city that maintains traditional Japanese culture.</p>
+    
+  </div>
+  <div class="main">
+    <header id  = "about">
+    <h1><b>Seputar Kyoto</b></h1>
+  
+  </header>
+    <br>
+  <header id  = "Album">
+      <h1>Album</h1>
+  </header>
+  </div>
+
+</div>
+
+<div class="footer">
+  <marquee style = "color:white">Copyright Johannes Alexander Putra</marquee>
+</div>
+
+</body>
+</html>
